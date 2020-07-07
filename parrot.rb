42e4f523e2bfed1563466d457d815828d10cafda
Austin Stanley <stanley.377@buckeyemail.osu.edu>
2020-07-07 18:29:31 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(string="Squawk!")
-  puts "#{string}"
-  return "#{string}"
-end
+# def parrot(string="Squawk!")
+#  puts "#{string}"
+#  return "#{string}"
+# end
 
 # this is the updated and more concise solution
 def parrot(string="Squawk!")
